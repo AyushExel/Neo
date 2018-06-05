53981ceee26b913a0796888a38307c72a8436179
AyushExel <thedeveloper.ayush@gmail.com>
2018-06-05 14:28:56 +0530

    Update documentation

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['_5f_5finit_5f_5f',['__init__',['../classnn_1_1nn.html#ad7304c7932970a07bf2869e97b79e0b5',1,'nn::nn']]]
+  ['neo',['Neo',['../md_README.html',1,'']]]
 ];
--- html/search/searchdata.js
+++ html/search/searchdata.js
@@ -1,27 +1,18 @@
 var indexSectionsWithContent =
 {
-  0: "_fin",
-  1: "n",
-  2: "n",
-  3: "_fi",
-  4: "n"
+  0: "n",
+  1: "n"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "classes",
-  2: "namespaces",
-  3: "functions",
-  4: "pages"
+  1: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Classes",
-  2: "Namespaces",
-  3: "Functions",
-  4: "Pages"
+  1: "Pages"
 };
 
