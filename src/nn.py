0177b0d5c653a4c2f924e24e5cc61adf3154dac2
Martin Uribe <mohhinder@gmail.com>
2018-06-05 06:52:46 -0500

    Rewrote comments

--- src/nn.py
+++ src/nn.py
@@ -1,130 +1,151 @@
-'''
+"""
 Class containing functionality to build and train neural networks
 Contains all activation and loss functions some other utility function.
-'''
+"""
 
-import numpy as np 
+import numpy as np
+
 
 class nn:
-    def __init__(self,layer_dimensions,activations):
-        '''
-        Initializes networks's weights and other useful variables
-        Variables-parameters=>contains weights of the layer in form {'Wi':[],'bi':[]}
-                  cache=>contains intermediate results as [[A[i-1],Wi,bi],[Zi]],where i
-                  is layer number.
-                  activations=> TO store the activation for each layer
-        '''
-        self.parameters = {} 
-        self.cache = []      
-        self.activations= []
+    def __init__(self, layer_dimensions, activations):
+        """
+        Initializes networks's weights and other useful variables.
+
+        Parameters contains weights of the layer in form {'Wi':[],'bi':[]}
+        Cache contains intermediate results as [[A[i-1],Wi,bi],[Zi]], where i
+        is layer number.
+
+        :param layer_dimensions:
+        :param activations: To store the activation for each layer
+        """
+        self.parameters = {}
+        self.cache = []
+        self.activations = []
 
         self.initialize_parameters(layer_dimensions)
         self.initialize_activations(activations)
-    
-    def initialize_parameters(self,layer_dimensions):
-        '''
-        Task -Random intialization of weights of a network described by given layer dimensions
-        inputs- layer_dimensions => dimensions to layers of the network
-        returns- none.
-        '''
-        for i in range(1,len(layer_dimensions)):
-            self.parameters['W'+str(i)] = np.random.randn(layer_dimensions[i],layer_dimensions[i-1])*0.01
-            self.parameters['b'+str(i)] = np.zeros((layer_dimensions[i],1))
-    
-    def initialize_activations(self,activations):
-        '''
-        Taks- Intialize the activation list 
-        Inputs- A list dscribing various activation units
-        Returns-none
-        '''
-        self.activations = activations
-    
-    def __linear_forward(self,A_prev,W,b):
-        '''
-        Task- Linear forward to the current layer using previous activations
-        inputs- A_prev=>Previous Layer's activation,
-                W,b=> Weights and biases for current layer
-        returns- linear cache and current calculated layer
-        '''
-        Z = W.dot(A_prev) + b
-        linear_cache= [A_prev,W,b]
-        return Z,linear_cache
 
-    def __activate(self,Z,n_layer):
-        '''
-        Task-Activate the given layer(Z) using the activation function specified by 'type'
-        inputs- Z=>layer to activate, type=>type of activation function to be used
-        returns- Activated layer and activation cache
-        Note: This function treats 1 as starting index! first layer's index is 1.
-        '''
+    def initialize_parameters(self, layer_dimensions):
+        """
+        Random initialization of weights of a network described by given layer
+        dimensions.
+
+        :param layer_dimensions: Dimensions to layers of the network
+        :return: None
+        """
+        for i in range(1, len(layer_dimensions)):
+            self.parameters["W" + str(i)] = (
+                np.random.randn(layer_dimensions[i],
+                                layer_dimensions[i - 1]) * 0.01
+            )
+            self.parameters["b" + str(i)] = np.zeros((layer_dimensions[i], 1))
+
+    def initialize_activations(self, activations):
+        """
+        Initialize the activation list.
+
+        :param activations: A list describing various activation units
+        :return: None
+        """
+        self.activations = activations
+
+    def __linear_forward(self, A_prev, W, b):
+        """
+        Linear forward to the current layer using previous activations.
+
+        :param A_prev: Previous Layer's activation
+        :param W: Weights for current layer
+        :param b: Biases for current layer
+        :return: Linear cache and current calculated layer
+        """
+        Z = W.dot(A_prev) + b
+        linear_cache = [A_prev, W, b]
+        return Z, linear_cache
+
+    def __activate(self, Z, n_layer):
+        """
+        Activate the given layer(Z) using the activation function specified by
+        'type'.
+
+        Note: This function treats 1 as starting index!
+              First layer's index is 1.
+
+        :param Z: Layer to activate
+        :param n_layer: Layer's index
+        :return: Activated layer and activation cache
+        """
         act_cache = [Z]
         act = None
-        if((self.activations[n_layer-1]).lower()=='relu'):
-            act = Z*(Z>0)
-        if((self.activations[n_layer-1]).lower()=='tanh'):
+        if (self.activations[n_layer - 1]).lower() == "relu":
+            act = Z * (Z > 0)
+        if (self.activations[n_layer - 1]).lower() == "tanh":
             act = np.tanh(Z)
-        if((self.activations[n_layer-1]).lower()=='sigmoid'):
-            act = 1/(1+np.exp(-Z))
-        
-        #assert(act!=None)
+        if (self.activations[n_layer - 1]).lower() == "sigmoid":
+            act = 1 / (1 + np.exp(-Z))
 
-        return act,act_cache
-    
-    def forward(self,input):
-        '''
-        Taks-To forward propagate the entire layer
-        inputs- input=> Contains the input to the Network
-        returns- Output of the network
-        '''
+        # assert(act!=None)
+
+        return act, act_cache
+
+    def forward(self, input):
+        """
+        To forward propagate the entire layer.
+
+        :param input: Contains the input to the Network
+        :return: Output of the network
+        """
         A = input
 
-        for i in range(1,int(len(self.parameters)/2)):
-            W = self.parameters['W'+str(i)]
-            b = self.parameters['b'+str(i)]
-            Z,linear_cache = self.__linear_forward(A,W,b)
+        for i in range(1, int(len(self.parameters) / 2)):
+            W = self.parameters["W" + str(i)]
+            b = self.parameters["b" + str(i)]
+            Z, linear_cache = self.__linear_forward(A, W, b)
 
-            A,act_cache = self.__activate(Z,i)
-            self.cache.append([linear_cache,act_cache])
-        
-        #For Last Layer
-        W = self.parameters['W'+str(int(len(self.parameters)/2))]
-        b = self.parameters['b'+str(int(len(self.parameters)/2))]
-        Z,linear_cache = self.__linear_forward(A,W,b)
-        if len(self.activations)==len(self.parameters)/2:
-            A,act_cache = self.__activate(Z,len(self.activations))
-            self.cache.append([linear_cache,act_cache])
+            A, act_cache = self.__activate(Z, i)
+            self.cache.append([linear_cache, act_cache])
+
+        # For Last Layer
+        W = self.parameters["W" + str(int(len(self.parameters) / 2))]
+        b = self.parameters["b" + str(int(len(self.parameters) / 2))]
+        Z, linear_cache = self.__linear_forward(A, W, b)
+        if len(self.activations) == len(self.parameters) / 2:
+            A, act_cache = self.__activate(Z, len(self.activations))
+            self.cache.append([linear_cache, act_cache])
         else:
             A = Z
-            self.cache.append([linear_cache,[None]])
+            self.cache.append([linear_cache, [None]])
 
         return A
 
-    def forward_upto(self,input,layer_num):
-        '''
-        Task- Calculates forward prop upto layer_num
-        Inputs- input=> Contains the input to the Network
-                layer_num=>Layer upto which forward prop is to be calculated
-        Returns- Activations of layer layer_num
-        '''
-        if layer_num == int(len(self.parameters)/2):
+    def forward_upto(self, input, layer_num):
+        """
+        Calculates forward prop upto layer_num.
+
+        :param input: Contains the input to the Network
+        :param layer_num: Layer up to which forward prop is to be calculated
+        :return: Activations of layer layer_num
+        """
+        if layer_num == int(len(self.parameters) / 2):
             return self.forward(input)
         else:
             A = input
-            for i in range(1,layer_num):
-                W = self.parameters['W'+str(i)]
-                b = self.parameters['b'+str(i)]
-                Z,linear_cache = self.__linear_forward(A,W,b)
+            for i in range(1, layer_num):
+                W = self.parameters["W" + str(i)]
+                b = self.parameters["b" + str(i)]
+                Z, linear_cache = self.__linear_forward(A, W, b)
 
-                A,act_cache = self.__activate(Z,i)
-                self.cache.append([linear_cache,act_cache])
+                A, act_cache = self.__activate(Z, i)
+                self.cache.append([linear_cache, act_cache])
             return A
-                
 
-#test run:
-data = np.random.randn(2,100)
-net = nn([2,15,2],['tanh','relu'])
-A = net.forward(data)
-print(A.shape)
 
-    
+def test_run():
+    # test run:
+    data = np.random.randn(2, 100)
+    net = nn([2, 15, 2], ["tanh", "relu"])
+    A = net.forward(data)
+    print(A.shape)
 
+
+if __name__ == "__main__":
+    test_run()
