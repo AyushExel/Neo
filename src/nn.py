fd7cd8e6de1c5a5ea94b81e78d83dcf3c4099315
AyushExel <thedeveloper.ayush@gmail.com>
2018-06-21 10:24:34 +0530

    Seperate network and optimzer
    -A new file 'optimizer.py' has been introduced, which will contain
    optimization algorithms
    -Gradient descenet optimer function has been removed from nn class to
    optimizer.py as a standalone function which takes one extra argument in
    form of nn object
    -This format will be maintained for all the optimzation algorithms

--- src/nn.py
+++ src/nn.py
@@ -265,45 +265,7 @@ class nn:
             self.grads['db'+str(l+1)] = db
             self.grads['dA'+str(l)] = dA_prev
     
-    def gradientDescent(self,input,mappings,alpha=0.001,lamb=None, epoch=100,print_at=5,prnt=True):
-        '''
-        Performs gradient descent on the given network setting the default value of epoch and alpha if not provided otherwise
-
-        :param input: input for neural net
-        :param mapping: Correct output of the function
-        :param alpha: Learning rate
-        :param lamb: Regularization parameter
-        :param epoch: Number of iterations
-        :param print_at: Print at multiples of 'print_at'
-        :param prnt: Print if prnt=true
-        '''
-
-        for i in range(epoch):
-            self.cache = []
-            prediction = self.forward(input)
-            loss_function = (self.cost_function).lower()
-            loss,regularization_cost = None,0
-            if loss_function == 'mseloss':
-                loss = self.MSELoss(prediction,mappings)
-            if loss_function == 'crossentropyloss':
-                loss = self.CrossEntropyLoss(prediction,mappings)
-
-            if lamb != None:
-                for params in range(len(self.cache)):
-                    regularization_cost = regularization_cost + np.sum(np.square(self.parameters['W'+str(params+1)]))
-                regularization_cost = (lamb/(2*input.shape[1]))*regularization_cost
-                
-            loss = loss + regularization_cost
-            self.lamb = lamb
-            if prnt and i%print_at==0 :
-                print('Loss at ',i, ' ' ,loss)
-            self.backward(prediction,mappings)
-            
-            for l in range(int(len(self.parameters)/2)):
-                self.parameters['W'+str(l+1)] = self.parameters['W'+str(l+1)] -alpha*self.grads['dW'+str(l+1)]
-                self.parameters['b'+str(l+1)] = self.parameters['b'+str(l+1)] -alpha*self.grads['db'+str(l+1)]
-
-    
+   
     def __str__(self):
         '''
         :Return: the network architecture and connectivity
