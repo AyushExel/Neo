8899d8ced08785b4a794e72062a4d8e2b1deb86d
AyushExel <thedeveloper.ayush@gmail.com>
2018-06-21 19:32:54 +0530

    Add implementation of stochaistic gradient descent
    -function-SGDOptimizer(input,mappings,net:nn.nn,mini_batch_size=64,alpha=0.001,lamb=None,
    epoch=5,print_at=5,prnt=True)
    
    -The API inputs follow same structure as the implementation of standard
    gradient descent function
    
    -documentation available

--- src/optimizer.py
+++ src/optimizer.py
@@ -44,6 +44,63 @@ def gradientDescentOptimizer(input,mappings,net:nn.nn,alpha=0.001,lamb=None, epo
             net.parameters['b'+str(l+1)] = net.parameters['b'+str(l+1)] -alpha*net.grads['db'+str(l+1)]
 
 
+def SGDOptimizer(input,mappings,net:nn.nn,mini_batch_size=64,alpha=0.001,lamb=None, epoch=5,print_at=5,prnt=True):
+    '''
+    Performs Stochaitic gradient descent on the given network
+    -Generates mini batches of given size using random permutation
+    -Uses gradient descent on each mini batch separately
+
+    :param input  : input for neural net
+    :param mapping: Correct output of the function
+    :param net    : nn.nn object which provides the network architecture
+    :param batch_size: Batch size to be used witch SGD
+    :param alpha  : Learning rate
+    :param lamb   : Regularization parameter
+    :param epoch  : Number of iterations
+    :param print_at: Print at multiples of 'print_at'
+    :param prnt   : Print if prnt=true
+    '''
+    batch_size = input.shape[1]
+    mini_batches = []
+    
+    permutation = list(np.random.permutation(batch_size))
+    shuffled_input = input[:,permutation]
+    shuffled_mappings = (mappings[:,permutation])
+
+    num_complete_batches = int(np.floor(batch_size/mini_batch_size))
+    
+    #Separate the complete mini_batches
+    for i in range(0,num_complete_batches):
+        mini_batch_input = shuffled_input[:,i*mini_batch_size:(i+1)*mini_batch_size]
+        mini_batch_mappings = shuffled_mappings[:,i*mini_batch_size:(i+1)*mini_batch_size]
+        mini_batch = (mini_batch_input,mini_batch_mappings)
+        mini_batches.append(mini_batch)
+    
+    #Separate the incomplete mini batch if any
+    if batch_size % mini_batch_size != 0:
+        mini_batch_input = shuffled_input[:,batch_size - num_complete_batches*mini_batch_size : batch_size]
+        mini_batch_mappings = shuffled_mappings[:,batch_size - num_complete_batches*mini_batch_size : batch_size]
+        mini_batch = (mini_batch_input,mini_batch_mappings)
+        mini_batches.append(mini_batch)
+    
+    for i in range(1,epoch+1):
+        for batches in range(len(mini_batches)):
+            gradientDescentOptimizer(input,mappings,net,alpha,lamb,epoch=1,prnt=False)
+        prediction = net.forward(input)
+        loss = None 
+        loss_function = net.cost_function.lower()
+        if loss_function == 'mseloss':
+            loss = net.MSELoss(prediction,mappings)
+        if loss_function == 'crossentropyloss':
+            loss = net.CrossEntropyLoss(prediction,mappings)
+        
+        if i%print_at == 0:
+            print('Loss at ', i , ' ' , loss)
+
+        
+
+
+    
 
 
     
