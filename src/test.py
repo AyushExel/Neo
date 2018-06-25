4710db0255087e919c0a849a7debd3ef18ca0a2b
AyushExel <thedeveloper.ayush@gmail.com>
2018-06-25 19:56:22 +0530

    Add test for SGD along with momentum

--- src/test.py
+++ src/test.py
@@ -6,7 +6,7 @@ import sklearn.datasets as datasets
 import nn
 import numpy as np
 import pandas as pd
-import optimizer
+from optimizer import optimizer
 
 def test_run():
     """
@@ -19,9 +19,9 @@ def test_run():
     print('Running a binary classification test')
 
     #Generate sample binary classification data
-    data = datasets.make_classification(n_samples=200,n_features=10,n_classes=2)
+    data = datasets.make_classification(n_samples=50000,n_features=10,n_classes=2)
     X= data[0].T
-    Y = (data[1].reshape(200,1)).T
+    Y = (data[1].reshape(50000,1)).T
     net = nn.nn([10,20,1],['relu','sigmoid'])
     net.cost_function = 'CrossEntropyLoss'
     print('net architecture :')
@@ -32,18 +32,28 @@ def test_run():
     output = net.forward(X)
     #Convert the probabilities to output values
     output = 1*(output>=0.5)
-    accuracy = np.sum(output==Y)/200
+    accuracy = np.sum(output==Y)/50000
     print('for gradient descenet \n accuracy = ' ,accuracy*100)
 
-    #Optimize using standard gradient descenet
+    #Optimize using SGD without momentum
     net = nn.nn([10,20,1],['relu','sigmoid'])
     net.cost_function = 'CrossEntropyLoss'
     optim = optimizer.SGDOptimizer
-    optim(X,Y,net,16,alpha=0.07,epoch=5,lamb=0.05,print_at=1)
+    optim(X,Y,net,128,alpha=0.07,epoch=5,lamb=0.05,print_at=1)
     output = net.forward(X)
     output = 1*(output>=0.5)
-    accuracy = np.sum(output==Y)/200
-    print('for stochaistic gradient descenet \n accuracy = ' ,accuracy*100)
+    accuracy = np.sum(output==Y)/50000
+    print('for stochaistic gradient descenet without momentum\n accuracy = ' ,accuracy*100)
+
+    #optimize using  SGD with momentum
+    net = nn.nn([10,20,1],['relu','sigmoid'])
+    net.cost_function = 'CrossEntropyLoss'
+    optim = optimizer.SGDOptimizer
+    optim(X,Y,net,128,alpha=0.07,epoch=5,lamb=0.05,print_at=1,momentum=0.9)
+    output = net.forward(X)
+    output = 1*(output>=0.5)
+    accuracy = np.sum(output==Y)/50000
+    print('for stochaistic gradient descenet with momentum\n accuracy = ' ,accuracy*100)
 
 
     
@@ -51,7 +61,7 @@ def test_run():
     #test run for regresssion problem:
     #Generate sample regression data
          
-    X = np.random.randn(1,200)
+    X = np.random.randn(1,50000)
     Y = X**2 
     net = nn.nn([1,10,1],['relu'])
     net.cost_function = 'MSELoss'
@@ -60,13 +70,22 @@ def test_run():
 
     #Optimize using standard gradient descenet
     print('for gradient descenet ')
-    optim(X,Y,net,alpha=0.01,epoch=300,lamb=0.05,print_at=100)
+    optim = optimizer.gradientDescentOptimizer
+    optim(X,Y,net,alpha=0.3,epoch=200,lamb=0.05,print_at=100)
 
     net = nn.nn([1,10,1],['relu'])
     net.cost_function = 'MSELoss'
-    #Optimize using stochaistic gradient descenet
+    #Optimize using stochaistic gradient descenet without momentum
     print('for stochaistic gradient descenet ')
-    optim(X,Y,net,alpha=0.3,epoch=20,lamb=0.05,print_at=1)
+    optim = optimizer.SGDOptimizer
+    optim(X,Y,net,alpha=0.3,epoch=10,lamb=0.05,print_at=1)
+
+    net = nn.nn([1,10,1],['relu'])
+    net.cost_function = 'MSELoss'
+    #Optimize using stochaistic gradient descenet with momentum
+    print('for stochaistic gradient descenet with momentum ')
+    optim = optimizer.SGDOptimizer
+    optim(X,Y,net,alpha=0.3,epoch=10,lamb=0.05,print_at=1,momentum=0.9)
     
 
 
