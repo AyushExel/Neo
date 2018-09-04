32926429b8702a7c0a43943bb2937f6a11212222
Ayush Chaurasia <thedeveloper.ayush@gmail.com>
2018-09-04 14:30:42 +0530

    Add test case for ADAM optimizer `

--- src/test.py
+++ src/test.py
@@ -19,9 +19,9 @@ def test_run():
     print('Running a binary classification test')
 
     #Generate sample binary classification data
-    data = datasets.make_classification(n_samples=50000,n_features=10,n_classes=2)
+    data = datasets.make_classification(n_samples=30000,n_features=10,n_classes=2)
     X= data[0].T
-    Y = (data[1].reshape(50000,1)).T
+    Y = (data[1].reshape(30000,1)).T
     net = nn.nn([10,20,1],['relu','sigmoid'])
     net.cost_function = 'CrossEntropyLoss'
     print('net architecture :')
@@ -32,7 +32,7 @@ def test_run():
     output = net.forward(X)
     #Convert the probabilities to output values
     output = 1*(output>=0.5)
-    accuracy = np.sum(output==Y)/50000
+    accuracy = np.sum(output==Y)/30000
     print('for gradient descenet \n accuracy = ' ,accuracy*100)
 
     #Optimize using SGD without momentum
@@ -42,7 +42,7 @@ def test_run():
     optim(X,Y,net,128,alpha=0.07,epoch=5,lamb=0.05,print_at=1)
     output = net.forward(X)
     output = 1*(output>=0.5)
-    accuracy = np.sum(output==Y)/50000
+    accuracy = np.sum(output==Y)/30000
     print('for stochaistic gradient descenet without momentum\n accuracy = ' ,accuracy*100)
 
     #optimize using  SGD with momentum
@@ -52,8 +52,18 @@ def test_run():
     optim(X,Y,net,128,alpha=0.07,epoch=5,lamb=0.05,print_at=1,momentum=0.9)
     output = net.forward(X)
     output = 1*(output>=0.5)
-    accuracy = np.sum(output==Y)/50000
+    accuracy = np.sum(output==Y)/30000
     print('for stochaistic gradient descenet with momentum\n accuracy = ' ,accuracy*100)
+
+    #optimize using  ADAM
+    net = nn.nn([10,20,1],['relu','sigmoid'])
+    net.cost_function = 'CrossEntropyLoss'
+    optim = optimizer.AdamOptimizer
+    optim(X,Y,net,alpha=0.07,epoch=80,lamb=0.05,print_at=5)
+    output = net.forward(X)
+    output = 1*(output>=0.5)
+    accuracy = np.sum(output==Y)/30000
+    print('for Adam:\n accuracy = ' ,accuracy*100)
 
 
     
@@ -61,7 +71,7 @@ def test_run():
     #test run for regresssion problem:
     #Generate sample regression data
          
-    X = np.random.randn(1,50000)
+    X = np.random.randn(1,60000)
     Y = X**2 
     net = nn.nn([1,10,1],['relu'])
     net.cost_function = 'MSELoss'
@@ -86,10 +96,15 @@ def test_run():
     print('for stochaistic gradient descenet with momentum ')
     optim = optimizer.SGDOptimizer
     optim(X,Y,net,alpha=0.3,epoch=10,lamb=0.05,print_at=1,momentum=0.9)
-    
 
+    net = nn.nn([1,10,1],['relu'])
+    net.cost_function = 'MSELoss'
+    #Optimize using stochaistic gradient descenet with momentum
+    print('for Adam ')
+    optim = optimizer.AdamOptimizer
+    optim(X,Y,net,alpha=0.3,epoch=70,lamb=0.05,print_at=5)
 
-
+ 
 
    
 
